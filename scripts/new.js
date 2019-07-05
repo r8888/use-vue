59eb54275ad509fa388ba29a3b30918abeab9a4f
r8888 <2664855141@qq.com>
2019-07-05 17:15:46 +0800

    copy tpls/* to /*

--- scripts/new.js
+++ scripts/new.js
@@ -1,4 +1,31 @@
+const fs = require("fs")
+const { resolve } = require("path")
 const dirName = process.argv[2]
 if (!dirName) {
   process.exit(0)
 }
+
+fs.mkdir(resolve(__dirname, `../src/${dirName}`), err => {
+  if (err) {
+    console.log(err)
+  } else {
+    fs.copyFile(
+      resolve(__dirname, "../tpls/index.html"),
+      resolve(__dirname, `../src/${dirName}/index.html`),
+      err => {
+        if (err) {
+          console.log(err)
+        }
+      }
+    )
+    fs.copyFile(
+      resolve(__dirname, "../tpls/app.js"),
+      resolve(__dirname, `../src/${dirName}/app.js`),
+      err => {
+        if (err) {
+          console.log(err)
+        }
+      }
+    )
+  }
+})
